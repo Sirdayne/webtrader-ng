696c0ae8e962aee33ca65242c506d7280467bc1a
Yernar <mussinyernar@gmail.com>
2021-12-13 17:07:24 +0600

    login in progress

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private router: Router) {
+    this.form = fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.form, ' FORM');
+    if (this.form.valid) {
+      this.router.navigateByUrl('/');
+    }
   }
 
 }
